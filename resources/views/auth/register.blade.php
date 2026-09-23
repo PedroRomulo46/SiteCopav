7ad3657b7c63ee8b554d51d2090aa16c90a1cbde
soliveira-09 <samueloliveira.code.com>
2026-09-22 21:05:35 -0300

    Customizando a view de login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
-        @csrf
 
+        @csrf
+        <div class="bg-[#79A961] p-5 rounded-sm">
         <!-- Nome -->
         <div>
-            <x-input-label for="nome" :value="__('Nome')" />
-
+            <x-input-label for="nome" :value="__('Nome')"/>
             <x-text-input
                 id="nome"
                 class="block mt-1 w-full"
@@ -16,17 +16,14 @@
                 autofocus
                 autocomplete="name"
             />
-
             <x-input-error
                 :messages="$errors->get('nome')"
                 class="mt-2"
             />
         </div>
-
         <!-- E-mail -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('E-mail')" />
-
+            <x-input-label for="email" :value="__('E-mail')"/>
             <x-text-input
                 id="email"
                 class="block mt-1 w-full"
@@ -36,16 +33,14 @@
                 required
                 autocomplete="username"
             />
-
             <x-input-error
                 :messages="$errors->get('email')"
                 class="mt-2"
             />
         </div>
-
         <!-- Senha -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Senha')" />
+            <x-input-label for="password" :value="__('Senha')"/>
 
             <x-text-input
                 id="password"
@@ -55,17 +50,14 @@
                 required
                 autocomplete="new-password"
             />
-
             <x-input-error
                 :messages="$errors->get('password')"
                 class="mt-2"
             />
         </div>
-
         <!-- Confirmar senha -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirmar senha')" />
-
+            <x-input-label for="password_confirmation" :value="__('Confirmar senha')"/>
             <x-text-input
                 id="password_confirmation"
                 class="block mt-1 w-full"
@@ -74,24 +66,25 @@
                 required
                 autocomplete="new-password"
             />
-
             <x-input-error
                 :messages="$errors->get('password_confirmation')"
                 class="mt-2"
             />
         </div>
+        </div>
 
         <div class="flex items-center justify-end mt-4">
+            <!-- Botão "já possui cadastro?" -->
             <a
-                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                href="{{ route('login') }}"
-            >
+                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-sm focus:outline-none"
+                href="{{ route('login') }}">
                 {{ __('Já possui cadastro?') }}
             </a>
-
-            <x-primary-button class="ms-4">
+            <!-- Botão Cadastrar -->
+            <button type="submit"
+            class="ms-4 px-4 py-2 bg-[#79A961] hover:bg-[#709b58] text-white font-semibold rounded-md shadow-sm">
                 {{ __('Cadastrar') }}
-            </x-primary-button>
+            </button>
         </div>
     </form>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,15 +13,12 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&icon_names=search,arrow_right_alt,account_circle,forum,home,arrow_back,palette,settings,logout&display=block" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
